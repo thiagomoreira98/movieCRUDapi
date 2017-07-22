1853a4c0d1af435056f348ff2c85268c6ca53e8f
Thiago Moreira <moreira.g.thiago@gmail.com>
2017-07-22 01:14:55 -0300

    get e post no mongo

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var app = require('express')();
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var Movie = require('./models/movie');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || '3000';
+
+var dbName = process.env.DB_NAME || 'movieDB';
+var dbHost = process.env.DB_HOST || 'localhost';
+var dbPort = process.env.DB_PORT || '27017';
+
+var dbConnect = 'mongodb://' + dbHost + ':' + dbPort + '/' + dbName;
+var promisse = mongoose.connect(dbConnect, {
+    useMongoClient: true
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,7 +27,24 @@ app.get('/ping', function(req, res){
 });
 
 app.post('/movies', function(req, res){
-    //var movie = new Movie(req.body);
-    res.json(req.body);
+    var movie = new Movie(req.body);
+    movie.save(function(err){
+        if(err){
+            console.log("err: " +err);
+            return res.send(err);
+        }
+
+        res.send({message: "Movie Added"});
+    });
 });
 
+app.get('/movies', function(req, res){
+    Movie.find(function(err, movies){
+        if(err){
+            console.log("err: " +err);
+            return res.send(err); 
+        }
+
+        res.json(movies);
+    });
+});
