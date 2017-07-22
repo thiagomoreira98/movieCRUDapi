662433c0519624cb11c8abe700eed2c9690735fe
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2017-07-22 00:17:43 -0300

    add model e config of ports

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
+var Movie = require('./models/movie');
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-app.listen(3000, function(){
-    console.log('SERVER ON PORT 3000');
+app.listen(port, function(){
+    console.log( 'SERVER ON PORT ' + port );
 });
 
 app.get('/ping', function(req, res){
     res.json({
         "pong": "OK"
     })
+});
+
+app.post('/movies', function(req, res){
+    //var movie = new Movie(req.body);
+    res.json(req.body);
 });
 
